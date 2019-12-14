0c2bfa8e569353e8580eb73eb7ab5b68ab512337
gitdrik <58667795+gitdrik@users.noreply.github.com>
2019-12-14 18:13:51 +0100

    Add files via upload

---
+++ advent14b_opti2.py
@@ -0,0 +1,42 @@
+import math
+import copy
+
+chemicals = {}
+with open('reactions.txt') as f:
+    for line in f:
+        line = line.replace(',','')
+        words = line.split()
+        reactants = []
+        for i in range((len(words)-3)//2):
+            reactants.append((words[i*2+1],int(words[i*2])))
+        chemicals[words[-1]] = [0, int(words[-2]), reactants]
+start = copy.deepcopy(chemicals)
+
+
+def produce(chemical, num):
+    if chemical == 'ORE':
+        return num
+    if chemicals[chemical][0] >= num:
+        chemicals[chemical][0] -= num
+        return 0
+    else:
+        ore = 0
+        need = num - chemicals[chemical][0]
+        get = chemicals[chemical][1]
+        kits = math.ceil(need / get)
+        for chem in chemicals[chemical][2]:
+            ore += produce(chem[0],chem[1]*kits)
+        chemicals[chemical][0] += chemicals[chemical][1]*kits - num
+        return ore
+
+ore = 0
+guess = 1000000
+while 1:
+    ore = produce('FUEL',guess)
+    chemicals = copy.deepcopy(start)
+    newguess = guess + (1000000000000-ore)*guess//(2*ore)
+    if newguess == guess:
+        print(guess)
+        break
+    else:
+        guess = newguess
