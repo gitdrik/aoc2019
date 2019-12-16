e18bb234fb32fe66ad7d5cdb7ac509b2185831b4
gitdrik <58667795+gitdrik@users.noreply.github.com>
2019-12-17 00:34:34 +0100

    Add files via upload

---
+++ advent16b.py
@@ -0,0 +1,20 @@
+with open('16.txt') as f:
+    s = f.read().strip()
+
+digs = [int(x) for x in s]
+offset = int(s[0:7])
+roffset = 10000*len(digs)-offset
+digs.reverse()
+long = digs*(roffset//len(digs)) + digs[:(roffset % len(digs))]
+
+for p in range(100):
+    phase = [long[0]]
+    for i in range(1,len(long)): 
+        phase.append((phase[i-1]+long[i]) % 10)
+    long = phase
+
+result = long[-8:]
+result.reverse()
+print(''.join(map(str, result)))
+    
+    
