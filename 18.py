1a202a74d430c89e6b7a4879325cd1628073902f
gitdrik <58667795+gitdrik@users.noreply.github.com>
2019-12-18 16:29:24 +0100

    Update 18.py

--- 18.py
+++ 18.py
@@ -6,4 +6,8 @@ small = chars & {chr(x) for x in range(ord('a'),ord('z')+1)}
 big = chars & {chr(x) for x in range(ord('A'),ord('Z')+1)}
 cave = caveraw.split('\n')
 
+def graph(y,x):
+    global cave
+    
+
 
