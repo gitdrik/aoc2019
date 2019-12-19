bd7881df0cefd856d73ff61ab1edbca2e975902d
gitdrik <58667795+gitdrik@users.noreply.github.com>
2019-12-19 16:49:16 +0100

    Update 18.py

--- 18.py
+++ 18.py
@@ -4,10 +4,20 @@ with open('18.txt') as f:
 chars = set(caveraw)
 small = chars & {chr(x) for x in range(ord('a'),ord('z')+1)}
 big = chars & {chr(x) for x in range(ord('A'),ord('Z')+1)}
+
 cave = caveraw.split('\n')
 
-def graph(y,x):
-    global cave
+q1 = [x[:41] for x in cave[:41]]
+q2 = [x[:41] for x in cave[:41]]
+
+
+def graph(y,x,cave):
+    pass
+
+cavegraph = graph(39,39,q1)
+cavegraph += graph(39,41,q2)
+cavegraph += graph(41,41,q3)
+cavegraph += graph(41,39,q4)
     
 
 
