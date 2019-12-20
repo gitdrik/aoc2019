7608b648b05cd1bdfdd6cb5a3d9b7a22e6049b30
gitdrik <58667795+gitdrik@users.noreply.github.com>
2019-12-20 09:47:13 +0100

    Add files via upload

---
+++ 14.py
@@ -0,0 +1,29 @@
+chemicals = {}
+with open('reactions.txt') as f:
+    for line in f:
+        line = line.replace(',','')
+        words = line.split()
+        reactants = []
+        for i in range((len(words)-3)//2):
+            reactants.append((words[i*2+1],int(words[i*2])))
+        chemicals[words[-1]] = [0, int(words[-2]), reactants]
+chemicals['ORE'] = [0, 0, []]
+
+def produce(chemical, num):
+    if chemical == 'ORE':
+        chemicals['ORE'][1] += num
+        chemicals['ORE'][0] = num
+        return
+    
+    while chemicals[chemical][0] < num:
+        for chem in chemicals[chemical][2]:
+            produce(chem[0],chem[1])
+        chemicals[chemical][0] += chemicals[chemical][1]
+        
+    chemicals[chemical][0] -= num
+    return
+
+produce('FUEL',1)
+print(chemicals['ORE'][1])
+        
+        
